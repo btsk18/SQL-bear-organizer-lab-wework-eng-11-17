d7a2a81a1e009712c040263ad307542599a485a7
Brandon Leeds <brandonleeds@gmail.com>
2018-01-08 00:58:17 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12, "M", "brown", "calm", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12, "M", "brown", "calm", 1),
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "brown", "calm", 1)
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "M", "brown", "calm", 1),
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "brown", "calm", 1),
